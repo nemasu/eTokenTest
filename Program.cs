58a917e2f5931256b90d6fba128624e79c1b4487
nemasu <nemasu@gmail.com>
2021-03-25 14:17:50 +0900

    Update Program.cs
    Remove old header.

--- Program.cs
+++ Program.cs
@@ -1,26 +1,3 @@
-﻿///////////////////////////////////////////////////////////////////////////////
-//
-// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-// PARTICULAR PURPOSE.
-//
-// Copyright (c) Microsoft Corporation. All rights reserved.
-//
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Sample to demonstrate how to create a simple certificate request
-//  using CertEnroll classes.
-//
-//  NOTE: This sample requires Visual Studio 2005. Create a project and
-//  in the menu click on Project -> Add Reference...
-//  this will pop a dialog. Click on the COM tab
-//  Select 'CertEnroll 1.0 Type Library' and click OK.
-//
-//  This will create an interop library which will be used by the C# code.
-//
-///////////////////////////////////////////////////////////////////////////////
-
 using System;
 
 
